4ad6f96dbfeb839f8a9e3ee1b936fe83e781b645
Vitor Gabriel <vitorlima3m@gmail.com>
2024-11-15 15:13:20 -0300

    Exibir contatos, search e paginação

--- contact/admin.py
+++ contact/admin.py
@@ -4,11 +4,12 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone','show',
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
+    list_editable = 'show',
     list_display_links = 'id', 'first_name',
 
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
+from django.db.models import Q
+from contact.models import Contact
+from django.core.paginator import Paginator
 
 def index (request):
+    contacts= Contact.objects.all().filter(show=True).order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Contatos - '
+    }
+    
     return render(
         request,
-        'contact/index.html' 
+        'contact/index.html',
+        context
+    )
+    
+def contact (request, contact_id):
+    single_contact= get_object_or_404(Contact.objects, pk=contact_id, show=True)
+    contact_name = f'{single_contact.first_name} {single_contact.last_name}'
+    context = {
+        'contact': single_contact,
+        'site_title': contact_name
+    }
+    
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
+    
+def search (request):
+    search_value = request.GET.get('q','').strip()
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts= Contact.objects.all()\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+            )\
+        .order_by('-id')
+        
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Contatos - '
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context
     )
