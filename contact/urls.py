b8ae7d6bf842a7788079728d9e37b1b4d45a2da3
Vitor Gabriel <vitorlima3m@gmail.com>
2024-11-15 16:00:50 -0300

    Criação View Contact Create

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+
 from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/',views.contact, name = 'contact'),
-    path('search/',views.search, name = 'search'),
-    path('',views.index, name = 'index' ),
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
---
+++ contact/views/contact_forms.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404,redirect
+from django.db.models import Q
+from contact.models import Contact
+from django.core.paginator import Paginator
+
+def create (request):
+
+    context = {
+       
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
