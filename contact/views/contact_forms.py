a7443112575c7c67d0c93fa0fd011de4564f4adf
Vitor Gabriel <vitorlima3m@gmail.com>
2024-11-16 06:02:25 -0300

    Inicio aos formulários

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,15 +2,26 @@ from django.shortcuts import render, get_object_or_404,redirect
 from django.db.models import Q
 from contact.models import Contact
 from django.core.paginator import Paginator
+from contact.forms import ContactForm
 
 def create (request):
-
+    if request.method == 'POST':
+        context = {
+        'form':ContactForm(request.POST)
+        }
+ 
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+        
     context = {
-       
+        'form': ContactForm()
     }
     
     return render(
-        request,
-        'contact/index.html',
-        context
-    )
+            request,
+            'contact/create.html',
+            context
+        )
