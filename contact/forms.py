30593ce77565a3ffbbbcc105fbe96de7904eedfb
Vitor Gabriel <vitorlima3m@gmail.com>
2024-11-16 16:17:56 -0300

    Validando Form, Criando Delete, inicio Imagens

--- contact/forms.py
+++ contact/forms.py
@@ -4,22 +4,65 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio do init',
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para seu usuário',
+    )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class' : 'classe-a',
+        #     'placeholder' : 'Escreva Aquis',
+        #     })    
     class Meta:
         model = models.Contact
         fields = (
             'first_name','last_name','phone',
-            )
+            'email','description','category',
+        )  
+        # widgets = {
+        #     'first_name' : forms.TextInput(
+        #         attrs={
+        #             'class' : 'classe-a',
+        #             'placeholder' : 'Escreva Aqui',
+        #         }
+        #     )
+        # }
+            
+        
         
     def clean(self):
         cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
         
-        self.add_error(
+        if first_name == 'ABC':
+            self.add_error(
             'first_name',
             ValidationError(
-                'Mensagem de erro',
+                'Veio do ADd error',
                 code='invalid'
             )
         )
-        
-        return super().clean()
-
+        return first_name
